9466724c6d33660e6da439d29891b37dfb56ca49
Apoxtrophe <cyggardner@gmail.com>
2024-08-19 16:45:57 -0600

    Added activation functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,35 @@ pub mod matrix;
 use matrix::*;
 
 #[derive(Clone, Debug)]
+pub enum ActivationFunction {
+  Sigmoid,
+  Tanh,
+  ReLU,
+}
+
+impl ActivationFunction {
+    fn apply(&self, x:f32) -> f32 {
+        match self {
+            ActivationFunction::Sigmoid => 1.0 / (1.0 + (-x).exp()),
+            ActivationFunction::Tanh => x.tanh(),
+            ActivationFunction::ReLU => x.max(0.0),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct Network {
-  pub network_layout: Vec<usize>,
-  pub cluster: Vec<Matrix>,
+  pub layout: Vec<usize>,
+  pub layers: Vec<Matrix>,
+  pub hidden_activation: ActivationFunction,
+  pub output_activation: ActivationFunction,
 }
 // Gell
 impl Network {
   pub fn new(
-    network_layout: Vec<usize>
+    network_layout: Vec<usize>,
+    hidden_activation: ActivationFunction,
+    output_activation: ActivationFunction,
   ) -> Self {
       let length = network_layout.len() - 1;
       let mut network: Vec<Matrix> = Vec::new();
@@ -27,14 +48,16 @@ impl Network {
       network.push(Matrix::zeros(network_layout[length], 1)); // Output layer
       
       Network {
-          network_layout,
-          cluster: network,
-      }
+        layout: network_layout,
+        layers: network,
+        hidden_activation,
+        output_activation,
+    }
   }
   pub fn to_data(
     &self,
   ) -> Vec<Vec<f32>> {
-    let cluster = &self.cluster;
+    let cluster = &self.layers;
     let mut result: Vec<Vec<f32>> = Vec::new();
     for i in 0..cluster.len(){
       result.push(cluster[i].data());
@@ -45,7 +68,7 @@ impl Network {
     &mut self,
     data: Vec<Vec<f32>>,
   ) {
-    let cluster = &mut self.cluster;
+    let cluster = &mut self.layers;
     for (i, matrix_data) in data.into_iter().enumerate() {
       let rows = cluster[i].rows;
       let cols = cluster[i].cols; 
@@ -58,31 +81,36 @@ impl Network {
   pub fn forward(
     &self, inputs: Vec<f32>
   ) -> Vec<f32> {
-    let cluster = &self.cluster;
-
-    // Convert the input Vec<f32> to a Matrix
-    let input_matrix = Matrix::new(cluster[0].rows, 1, inputs);
-
-    let mut input = input_matrix; // Start with the input matrix
+    let layers = &self.layers;
+    let mut input_matrix = Matrix::new(layers[0].rows, 1, inputs);
 
     // Iterate through each hidden layer
-    for i in (1..cluster.len() - 3).step_by(2) {
-        let Z = (cluster[i].mul(&input)).add(&cluster[i + 1]);
-        input = Z.relu();
+    for i in (1..layers.len() - 3).step_by(2) {
+        let Z = (layers[i].mul(&input_matrix)).add(&layers[i + 1]);
+        input_matrix = self.apply_activation(&Z, &self.hidden_activation);
     }
 
     // Last hidden layer to output layer
-    let Z_last = (cluster[cluster.len() - 3].mul(&input)).add(&cluster[cluster.len() - 2]);
+    let Z_last = (layers[layers.len() - 3].mul(&input_matrix)).add(&layers[layers.len() - 2]);
 
-    let A_last = Z_last; // For regression, leave it as is
+    let A_last = self.apply_activation(&Z_last, &self.output_activation);
 
     A_last.data() // Return the final output
   }
+  
+  fn apply_activation(&self, matrix: &Matrix, activation: &ActivationFunction) -> Matrix {
+        match activation {
+            ActivationFunction::ReLU => matrix.relu(),
+            ActivationFunction::Sigmoid => matrix.sigmoid(),
+            ActivationFunction::Tanh => matrix.tanh(),
+        }
+    }
+  
   pub fn display(
     &self,
   )
   {
-    let cluster = &self.cluster;
+    let cluster = &self.layers;
     for i in 0..cluster.len(){
       if i == cluster.len() -1{
         println!("{} Output Neurons:  :: {:?}", i, cluster[i]);
@@ -98,11 +126,12 @@ impl Network {
       }
     }
   }
+  
   pub fn mutate(&mut self, factor: f32) {
       use rand::Rng;
       let mut rng = rand::thread_rng();
   
-      for matrix in &mut self.cluster {
+      for matrix in &mut self.layers {
           for i in 0..matrix.data.len() {
               let mutation = rng.gen_range(-factor..factor);
               matrix.data[i] += mutation;
